5daf3ff792bf088216d07026f64409b73a59c340
minh <65264453+lehoangminhfpt2020@users.noreply.github.com>
2020-09-23 05:59:50 +0700

    Final
    Final PJ

---
+++ js/baf.js
@@ -0,0 +1,4 @@
+// $('#myList a[href="#profile"]').tab('show') // Select tab by name
+// $('#myList a:first-child').tab('show') // Select first tab
+// $('#myList a:last-child').tab('show') // Select last tab
+// $('#myList a:nth-child(3)').tab('show') // Select third tab
